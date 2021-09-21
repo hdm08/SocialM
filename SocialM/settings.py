dc33823f6a05d05ee1415fec7e2db27dc9cdd42b
hdm08 <65184650+hdm08@users.noreply.github.com>
2021-09-21 22:13:36 +0530

    Update settings.py

--- SocialM/settings.py
+++ SocialM/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-STATICFILES_DIRS = [BASE_DIR/'static']
+STATIC_ROOT = BASE_DIR/"static"
 
 STATIC_URL = '/static/'
 
