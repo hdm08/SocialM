deb9b3db4cc7321782f71f390f90b547ee19e41b
hdm08 <65184650+hdm08@users.noreply.github.com>
2021-09-21 21:34:01 +0530

    Social Media

---
+++ main/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Comment, SubComment
+admin.site.register(Comment)
+admin.site.register(SubComment)
+# admin.site.register(Comment)
+# admin.site.register(SubComment)
