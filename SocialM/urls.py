f78255d32417a840a049796f8751f7d24c0e68a5
hdm08 <65184650+hdm08@users.noreply.github.com>
2021-09-23 17:06:49 +0530

    Update urls.py

--- SocialM/urls.py
+++ SocialM/urls.py
@@ -4,14 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main.urls')),
-    path('user/', include('user.urls')),
+        path('/', include('main.urls')),
+	path('admin/', admin.site.urls),
+   	path('user/', include('user.urls')),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
-
-]
+    ]
 
 if settings.DEBUG:
 	urlpatterns+= static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
